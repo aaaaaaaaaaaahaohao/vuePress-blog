e1c247d1825b38a23cffbf8965edfa16784f7c59
haohao <1050532303@qq.com>
2020-08-17 15:51:27 +0800

    【修改】bug fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,7 +12,7 @@ module.exports = {
       ref  : 'origin/master',
       repo : 'git@github.com:aaaaaaaaaaaahaohao/vuePress-blog.git',
       path : '/blog',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+      'post-deploy' : 'npm install && npm run build'
     }
   }
 };
