02718615fd62d1250d7974b14c7803304cc28fa3
haohao <1050532303@qq.com>
2020-08-30 11:25:20 +0800

    【修改】搭建个人博客

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,7 @@ module.exports = {
   title: 'my blog',
   description: '我的个人网站',
   head: [ // 注入到当前页面的 HTML <head> 中的标签
-    ['link', { rel: 'icon', href: '/logo.jpg' }], // 增加一个自定义的 favicon(网页标签的图标)
+    ['link', { rel: 'icon', href: '/logo.png' }], // 增加一个自定义的 favicon(网页标签的图标)
   ],
   markdown: {
     lineNumbers: false // 代码块显示行号
@@ -15,6 +15,9 @@ module.exports = {
       },
       {
         title: '微信小程序', children: ['/document/大数据setData性能优化']
+      },
+      {
+        title: '有趣实践', children: ['/document/搭建个人博客']
       }
     ], // 侧边栏配置
     sidebarDepth: 2, // 侧边栏显示2级
